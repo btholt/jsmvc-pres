77f0fe4ca6d97346c2d47afb0170d0585576ab4a
Brian Holt <bholt@reddit.com>
2014-02-09 16:19:19 -0700

    Initial commit: Angular.

---
+++ ng-todo/js/ngTodoCtrl.js
@@ -0,0 +1,20 @@
+ngTodo.controller('NgTodoCtrl', function NgTodoCtrl($scope) {
+  $scope.todos = [
+    { val: 'Make a sweet app.', completed: false },
+    { val: 'Present at Fluent.', completed: false },
+    { val: 'Learn Angular.', completed: true },
+    { val: 'Do cool things.', completed: true },
+    { val: 'Another goal.', completed: false }
+  ];
+
+  $scope.addNewTask = function() {
+    $scope.todos.unshift({ completed: false, val: $scope.newTask });
+    $scope.newTask = '';
+  };
+
+  $scope.clearCompleted = function() {
+    $scope.todos = $scope.todos.filter(function(el) {
+      return !el.completed;
+    });
+  }
+});
